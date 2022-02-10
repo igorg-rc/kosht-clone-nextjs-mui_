796553b3d557282b5ef6ba0bc76232edd5648c08
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2022-02-10 14:07:02 +0200

    changed images settings in next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 const { i18n } = require('./next-i18next.config')
-
+ 
 module.exports = {
   i18n,
-  optimizeFonts: false,
   images: {
-    domains: [
-      '193.46.199.82',
-      'assets.vercel.com', 
-      'placehold.it', 
-      'images.unsplash.com'
-    ],
-    formats: ['image/avif', 'image/webp'],
+    domains: ["images.unsplash.com", "193.46.199.82"]
   },
+  pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
+  target: "experimental-serverless-trace",
 }
